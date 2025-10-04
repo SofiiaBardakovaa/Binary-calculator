f11e65e70c1c58fa9d2d2d3d197a7707f1fff668
Софія Бардакова <sofiia.bardakova@nure.ua>
2025-10-04 21:00:05 +0300

    Розроблено операції множення та ділення. Завершено

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ def check_binary(num: str) -> bool:
 def add_binary(a: str, b: str) -> str:
     return bin(int(a, 2) + int(b, 2))[2:]
 
+def sub_binary(a: str, b: str) -> str:
+    if int(a, 2) < int(b, 2):
+        return "Помилка: результат від’ємний"
+    return bin(int(a, 2) - int(b, 2))[2:]
+
+def mul_binary(a: str, b: str) -> str:
+    return bin(int(a, 2) * int(b, 2))[2:]
+
+def div_binary(a: str, b: str):
+    if b == "0":
+        return "Помилка: ділення на нуль"
+    q = int(a, 2) // int(b, 2)
+    r = int(a, 2) % int(b, 2)
+    return f"Частка: {bin(q)[2:]}, залишок: {bin(r)[2:]}"
+
 the_end = False
 while not the_end:
     a = input("Введіть перше двійкове число: ")
@@ -23,6 +38,12 @@ while not the_end:
 
     if op == "+":
         print("Результат:", add_binary(a, b))
+    elif op == "-":
+        print("Результат:", sub_binary(a, b))
+    elif op == "*":
+        print("Результат:", mul_binary(a, b))
+    elif op == "/":
+        print("Результат:", div_binary(a, b))
 
     ask_to_continue = input("Бажаєте продовжити? (так/ні): ").lower()
     if ask_to_continue == "ні":
